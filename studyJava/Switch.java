e7dce2c215b929ffd1e7da294903f6694c27ca4c
SkySlime <39089488+SkySlime@users.noreply.github.com>
2020-12-14 12:51:54 +0700

    study switch case: break; default:
    from https://www.w3schools.com/java/java_switch.asp

---
+++ studyJava/Switch.java
@@ -0,0 +1,17 @@
+import javax.print.event.PrintJobListener;
+
+public class Switch {
+    public static void main(String[] args) {
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("No match");
+        }
+    }
+}
