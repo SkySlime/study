9f86e71a7a84820f2f9072da2ab176053cb55470
SkySlime <39089488+SkySlime@users.noreply.github.com>
2020-12-15 16:11:56 +0700

    main calling the subclass
    from https://www.w3schools.com/java/java_modifiers.asp

---
+++ studyJava/OOP1_classesAndObject/OOP5_modifiers_main.java
@@ -0,0 +1,6 @@
+public class OOP5_modifiers_main {
+    public static void main(String[] args) {
+        OOP5_modifiers_subclass instanceKun = new OOP5_modifiers_subclass();
+        instanceKun.study(); // call abstract method
+    }
+}
