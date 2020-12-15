508e556fd8555a9f26b1bda3da290fb37c60e9a3
SkySlime <39089488+SkySlime@users.noreply.github.com>
2020-12-15 14:23:24 +0700

    class that contains main method
    from https://www.w3schools.com/java/java_classes.asp

---
+++ studyJava/OOP1_classesAndObject/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        Classes myInstance = new Classes();
+        // created an instance from another class!!!
+        // can be done directly if that file is in the same directory
+        System.out.println(myInstance.x);
+    }
+}
