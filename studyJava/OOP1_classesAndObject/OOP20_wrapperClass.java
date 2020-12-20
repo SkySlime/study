77bd482c90b67f1d0dafe66caf1d7be6b5d2674e
SkySlime <39089488+SkySlime@users.noreply.github.com>
2020-12-20 11:23:59 +0700

    study WrapperClass of primitive data structure
    from https://www.w3schools.com/java/java_wrapper_classes.asp

---
+++ studyJava/OOP1_classesAndObject/OOP20_wrapperClass.java
@@ -0,0 +1,9 @@
+public class OOP20_wrapperClass {
+    public static void main(String[] args) {
+        Integer myInt = 5;
+        int myint = 4;
+        System.out.println(myInt.toString().length());
+        System.out.println(myint);
+        // primitive int cannot use the method toString()
+    }
+}
